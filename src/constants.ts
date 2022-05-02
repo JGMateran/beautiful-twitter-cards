b14106fd41da4df974de69685665c2e07747e390
Jose Gregorio Materan <josemateran1998@gmail.com>
2022-05-02 17:10:59 -0400

    fix: a correct link was added for the repository

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const TWITTER_URL = 'https://www.twitter.com/reburn_dev'
 export const KOFI_URL = 'https://www.ko-fi.com/reburn'
-export const GITHUB_REPOSITORY_URL = 'https://www.github.com/JGMatern/beautifull-twitter-cards'
+export const GITHUB_REPOSITORY_URL = 'https://github.com/JGMateran/beautiful-twitter-cards'
